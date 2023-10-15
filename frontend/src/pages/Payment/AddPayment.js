64787b9c75427e974c69cff4e798ea204e428edc
jxnuk <it21318870@my.sliit.lk>
2023-10-16 00:13:58 +0530

    delete paymentupdate

--- frontend/src/pages/Payment/AddPayment.js
+++ frontend/src/pages/Payment/AddPayment.js
@@ -120,25 +120,3 @@ export default function AddPayment() {
         </div>
     );
 }
-export default function Payments() {
-    return (
-      <div>
-        <div className='container mt-5'>
-          <h2 className='text-start'>Payments</h2>
-          <div className='row d-flex justify-content-center align-middle h-100 mt-5'>
-            <div className='col-6 shadow-lg p-3 mb-5 bg-white rounded'>
-              <Link to='/refunds' className='btn btn-primary mt-5 col-12'>
-                Refunds
-              </Link>
-              <Link to='/payment-history' className='btn btn-primary mt-5 col-12'>
-               Payment History
-              </Link>
-              <Link to='/reports' className='btn btn-primary mt-5 col-12'>
-                Reports
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
