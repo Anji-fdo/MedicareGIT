e1ec8f2b55ac4125044abf474767c4f934db614a
AdZeRo24 <110715146+IT21822780@users.noreply.github.com>
2023-10-17 10:05:22 +0530

    invEdit

--- frontend/src/pages/Inventory/EditInventory.js
+++ frontend/src/pages/Inventory/EditInventory.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
+//Edit inventory
 export default function EditInventory() {
     const { id } = useParams();
     const navigate = useNavigate();
